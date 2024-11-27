48f49a399d84844e24800c5e154af29fde21dbe9
unknown <esinamseraphine7@gmail>
2024-11-27 04:53:12 -0800

    Adding changes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+alert("Hello");
+let name=prompt("what is your name");
+let age=prompt("what is your age");
+let email=prompt("what is your email");
+alert(`my name is ${name} i am ${age} years old my email is ${email}`)
+
+
+
+
+
+
